3ad12c6c678c3a98e0fd004792fbf083461d32d5
Miguel Martinez <miguelmartinez@Miguels-MacBook-Pro.local>
2022-09-01 14:17:39 -0400

    Separate styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,6 @@
 import React, { useMemo, useState } from 'react';
 import { splitNextLines, transpileCode } from './helpers';
-
-const styles = {
-  container: {
-    padding: '20px',
-    height: '90rem',
-  },
-  subContainer: {
-    display: 'flex',
-    justifyContent: 'space-evenly',
-    height: '80%',
-  },
-  codeContainer: {
-    border: '1px solid #ccc',
-    borderRadius: '5px',
-    borderShadow: '5px 5px 5px #ccc',
-    backgroundColor: '#ffffff',
-    width: '45%',
-  },
-  outputContainer: {
-    border: '1px solid #ccc',
-    borderRadius: '5px',
-    paddingLeft: '10px',
-    paddingRight: '10px',
-    backgroundColor: '#ffffff',
-    borderShadow: '5px 5px 5px #ccc',
-    width: '45%',
-  },
-  textarea: {
-    border: 'none',
-    outline: 'none',
-    width: '98%',
-    height: '95%',
-    maxWidth: '98%',
-    overlay: 'none',
-    borderRadius: '5px',
-  },
-};
+import styles from './styles';
 
 const App = () => {
   const [textareaValue, setTextareaValue] = useState<string>('# Markdown syntax guide');
@@ -51,6 +15,7 @@ const App = () => {
 
   return (
     <div style={styles.container}>
+      <h2 style={{ textAlign: 'center' }}>Markdown Previewer</h2>
       <div style={styles.subContainer}>
         <div style={styles.codeContainer}>
           <textarea style={styles.textarea} onChange={handleChange} value={textareaValue} />
---
+++ src/styles.ts
@@ -0,0 +1,38 @@
+const styles = {
+  container: {
+    padding: '20px',
+    height: '90rem',
+  },
+  subContainer: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    height: '80%',
+  },
+  codeContainer: {
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    borderShadow: '5px 5px 5px #ccc',
+    backgroundColor: '#ffffff',
+    width: '45%',
+  },
+  outputContainer: {
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    paddingLeft: '10px',
+    paddingRight: '10px',
+    backgroundColor: '#ffffff',
+    borderShadow: '5px 5px 5px #ccc',
+    width: '45%',
+  },
+  textarea: {
+    border: 'none',
+    outline: 'none',
+    width: '98%',
+    height: '95%',
+    maxWidth: '98%',
+    overlay: 'none',
+    borderRadius: '5px',
+  },
+};
+
+export default styles;
